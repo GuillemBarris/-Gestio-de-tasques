d39c45554605fbf6384a3b26e85577f0b3ac5353
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-05-27 18:34:41 +0200

    Posar El titol, la descripcio curta, la descripcio llarga la Data i poderla seleccionar a partir de un boto

--- app/src/main/java/com/example/gestiodetasques/CrearTasca.kt
+++ app/src/main/java/com/example/gestiodetasques/CrearTasca.kt
@@ -1,8 +1,10 @@
 package com.example.gestiodetasques
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gestiodetasques.databinding.CrearTascaBinding
+import java.util.Calendar
 
 class CrearTasca : AppCompatActivity(){
     private lateinit var binding: CrearTascaBinding
@@ -12,6 +14,21 @@ class CrearTasca : AppCompatActivity(){
 
         binding = CrearTascaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnPickDateTime.setOnClickListener {
+            val dataPickerDialog = DatePickerDialog(
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    val selectedDate = "$dayOfMonth/${monthOfYear + 1}/$year"
+                    binding.DataSeleccionada.text = selectedDate
+
+                },
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+            )
+            dataPickerDialog.show()
+        }
 
 
     }
