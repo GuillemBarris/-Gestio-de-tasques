9554c38cf5afb6bcd417259e6f28067551ecf142
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-05-28 19:03:54 +0200

    Fer que l'usuari pugui Agafar una imatge del seu mobil

--- app/src/main/java/com/example/gestiodetasques/CrearTasca.kt
+++ app/src/main/java/com/example/gestiodetasques/CrearTasca.kt
@@ -1,14 +1,21 @@
+
+
 package com.example.gestiodetasques
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gestiodetasques.databinding.CrearTascaBinding
 import java.util.Calendar
 
-class CrearTasca : AppCompatActivity(){
+class CrearTasca : AppCompatActivity() {
     private lateinit var binding: CrearTascaBinding
 
+    companion object {
+        private const val SELECT_IMAGE_REQUEST_CODE = 100
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -28,8 +35,22 @@ class CrearTasca : AppCompatActivity(){
                 Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
             )
             dataPickerDialog.show()
+
+        }
+        binding.btnSelectImg.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, SELECT_IMAGE_REQUEST_CODE)
         }
 
 
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SELECT_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+            binding.ImageView.setImageURI(selectedImageUri)
+        }
+    }
+
 }
